52a5ec51c1438b8474857e62c7d41407301ef4a2
Alan <Alan_manuel_gonzalez@hotmail.com>
2023-09-30 17:19:32 -0300

    feat: animation turn

--- Player.gd
+++ Player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const SPEED_MULTIPLIER = 1.5
+const FLIP_ANIMATION_SPEED = 15.0 
+
+var target_scale = Vector2(1, 1)
+var current_scale = Vector2(1, 1)
 
 func _ready():
 	$moveAnimatedSprite.pause()
@@ -16,9 +20,16 @@ func _physics_process(delta):
 	if direccion.length() > 0:
 		$moveAnimatedSprite.play()
 		velocity = direccion.normalized() * SPEED * multiplicador
-		$moveAnimatedSprite.flip_h = direccion.x < 0
+		
+		if direccion.x < 0:
+			target_scale.x = -1
+		else:
+			target_scale.x = 1
 	else:
 		$moveAnimatedSprite.stop()
 		velocity = Vector2(0, 0)
+		
+	current_scale.x = lerp(current_scale.x, target_scale.x, FLIP_ANIMATION_SPEED * delta)
+	$moveAnimatedSprite.scale.x = current_scale.x
 		
 	move_and_slide()
