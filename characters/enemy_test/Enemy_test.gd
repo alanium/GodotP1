25d9c7464da5bc4d3162f8ada298cbf2b419f01b
Alan <Alan_manuel_gonzalez@hotmail.com>
2023-10-01 15:48:38 -0300

    fix: enemy_delay

--- characters/enemy_test/Enemy_test.gd
+++ characters/enemy_test/Enemy_test.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
+var speed = 100
 
 func _on_detection_area_body_entered(body):
 	if body.name == 'Player':
-		print("¡El jugador ha entrado en el área de detección del enemigo!")
-
+		pass
 
 func _on_detection_area_body_exited(body):
 	if body.name == 'Player':
-		print("¡El jugador ha salido en el área de detección del enemigo!")
+		pass
+
+func _process(delta):
+	if is_player_in_detection_area():
+		var player = get_node("/root/World/Player")
+		var direction = (player.global_position - global_position).normalized() 
+		var distance = global_position.distance_to(player.global_position)
+		
+		# Si la distancia entre el enemigo y el jugador es menor que cierto valor, alejar al enemigo del jugador
+		if distance < 85:  # Ajusta este valor según lo que funcione mejor para tu juego
+			velocity = -direction * speed  # Moverse en dirección opuesta al jugador
+		else:
+			velocity = direction * speed  # Moverse hacia el jugador
+
+		move_and_slide()
+
+func is_player_in_detection_area():
+	var detection_area = $DetectionArea
+	var overlapping_bodies = detection_area.get_overlapping_bodies()
+	for body in overlapping_bodies:
+		if body.name == "Player":
+			return true
+	return false
